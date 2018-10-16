75a6f5c27682b1e82759447def79b12415828d59
Jason <jason@Jasonde-MacBook-Pro.local>
2018-10-16 17:21:06 +0800

    Fetching data from a remote API

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends Component {
-	
+
 	state = {
-		pageHeader: 'Naming Contests'
+		pageHeader: 'Naming Contests',
+		contests: []
+	};
+
+	componentDidMount() {
+		axios.get('/api/contests')
+		.then(res => {
+			this.setState({
+				contests: res.data.contests
+			})
+		})
+		.catch(console.error)
+	}
+
+	componentWillUnmount() {
+		// clean timers, listeners
 	}
 
 	render() {
 		return (
 			<div className="App">
 				<Header message={ this.state.pageHeader } />
-				<div>...</div>
+				<div>
+					{this.state.contests.map(contest =>
+						<ContestPreview key={ contest.id } {...contest} />
+					)}
+				</div>
 			</div>
 		)
 	}
