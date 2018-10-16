47ad9f56e068a3b67f5aad911e620728ad575916
Jason <jason@Jasonde-MacBook-Pro.local>
2018-10-16 10:12:25 +0800

    React component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App'
 
 ReactDOM.render(
-	React.createElement('h2', null, 'Hello React'),
+	<App />,
 	document.getElementById('root')
 );
